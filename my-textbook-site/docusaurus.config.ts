bc016e385d9ec66f8a3f876742ae5ae424087f1f
Hafiza Alina Yasmeen <alinayasmeen166@gmail.com>
2025-12-06 00:23:52 +0500

    Update docusaurus.config.ts

--- my-textbook-site/docusaurus.config.ts
+++ my-textbook-site/docusaurus.config.ts
@@ -151,7 +151,6 @@ const config: Config = {
         {
           title: 'More',
           items: [
-            { label: 'Blog', to: '/blog' },
             { label: 'GitHub', href: 'https://github.com/facebook/docusaurus' },
           ],
         },
